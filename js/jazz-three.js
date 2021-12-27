370f7c09873f49d13d4facc16e6bf58865d0cb5c
Nara Yaralyan <narayaralian@gmail.com>
2021-12-26 22:07:17 -0800

    Redesign navigation

--- js/jazz-three.js
+++ js/jazz-three.js
@@ -169,3 +169,25 @@ render();
 
 //animate on hover
 window.addEventListener('mousemove', onMousemove);
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
